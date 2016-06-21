054fab8a3d06d397d7190de7be0675a0ec2567af
Christopher Lorton <clorton@intven.com>
2016-06-21 14:18:53 -0700

    Support for STI and HIV migration.

--- Eradication/Relationship.h
+++ Eradication/Relationship.h
@@ -49,6 +49,7 @@ namespace Kernel
             virtual const suids::suid& GetSuid() const override;
             virtual const std::string& GetPropertyKey() override;
             virtual const std::string& GetPropertyName() const override;
+            virtual unsigned int GetSlotNumberForPartner( bool forPartnerB ) const override;
             virtual const tRelationshipMembers GetMembers() const override;
             virtual bool IsDiscordant() const override;
             virtual float GetTimer() const override;
@@ -60,6 +61,8 @@ namespace Kernel
             virtual suids::suid GetMalePartnerId() const override;
             virtual suids::suid GetFemalePartnerId() const override;
             virtual ExternalNodeId_t GetOriginalNodeId() const override;
+            virtual bool IsMalePartnerAbsent() const override;
+            virtual bool IsFemalePartnerAbsent() const override;
 
             virtual float GetCoitalRate() const override;
             virtual ProbabilityNumber getProbabilityUsingCondomThisAct() const override;
@@ -193,6 +196,28 @@ namespace Kernel
 #pragma warning( push )
 #pragma warning( disable: 4251 ) // See IdmApi.h for details
             DECLARE_SERIALIZABLE(MarriageRelationship);
+#pragma warning( pop )
+    };
+
+    class CommercialRelationship : public Relationship
+    {
+        public:
+            friend class RelationshipFactory;
+            DECLARE_QUERY_INTERFACE()
+
+        protected:
+            CommercialRelationship();
+            CommercialRelationship( const suids::suid& rRelId,
+                                       IRelationshipManager* pRelMan,
+                                       IRelationshipParameters* pParams, 
+                                       IIndividualHumanSTI* male_partner, 
+                                       IIndividualHumanSTI* female_partner );
+
+            virtual Relationship* Clone() override;
+
+#pragma warning( push )
+#pragma warning( disable: 4251 ) // See IdmApi.h for details
+            DECLARE_SERIALIZABLE(CommercialRelationship);
 #pragma warning( pop )
     };
 }
